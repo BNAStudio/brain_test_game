1254de4428aaedd85f0bde38767fd382d29dd0b9
jm2555 <114376247+jm2555@users.noreply.github.com>
2023-06-11 18:09:13 -0500

    Código complementario para respuesta en enteros

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,18 +8,19 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         int puntaje = 0;
-        
+
         // El ciclo se repite mientras el jugador responda de forma correcta
 
         while (true) {
             int termino1 = random.nextInt(100); // Genera un número aleatorio entre 0 y 99
             int termino2 = random.nextInt(100); // Genera un número aleatorio entre 0 y 99
-            int resultado;
-            
+            double resultado;
+            int respuesta;
+
             int operador = random.nextInt(4);  // Genera un operador aleatorio (+, -, *, /), cada numero representa un caso en la estructura Switch
             char operadorChar;
 
-            // Define que operacion se imprimira
+            // Define qué operación se imprimirá
             switch (operador) {
                 case 0 -> {
                     resultado = termino1 + termino2;
@@ -37,7 +38,7 @@ public class Main {
                     if (termino2 == 0) {  // Evita división por cero
                         continue;
                     }
-                    resultado = termino1 / termino2;
+                    resultado = (double) termino1 / termino2;
                     operadorChar = '/';
                 }
                 default -> {
@@ -46,17 +47,23 @@ public class Main {
             }
 
             System.out.println("¡Bienvenido Brain Test!");
-            System.out.print("Cuánto es " + termino1 + " " + operadorChar + " " + termino2 + "? ");
+            System.out.print("¿Cuánto es " + termino1 + " " + operadorChar + " " + termino2 + "? ");
 
             System.out.println("Responde correctamente para acumular puntos.");
-            int respuesta = scanner.nextInt();
 
-            if (respuesta == resultado) { // Valida si la respuesta del usuario es igual al resultado
-                puntaje += 5;
-                System.out.println("¡Respuesta correcta! Puntaje actual: " + puntaje);
+            if (scanner.hasNextInt()) {
+                respuesta = scanner.nextInt();
+                if (Math.round(resultado) == respuesta) { // Compara el resultado redondeado con la respuesta del jugador
+                    puntaje += 5;
+                    System.out.println("¡Respuesta correcta! Puntaje actual: " + puntaje);
+                } else {
+                    System.out.println("Respuesta incorrecta. Fin del juego.");
+                    break;
+                }
             } else {
-                System.out.println("Respuesta incorrecta. Fin del juego.");
-                break;
+                System.out.println("Error: La respuesta debe ser un número entero. Intenta nuevamente.");
+                scanner.next(); // Limpiar la entrada no válida
+                continue;
             }
         }
 
